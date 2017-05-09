a53e8e572253d1a3a914131d0dd8c9e44a8824cd
Rob Lugton <rob.lugton@192-168-1-91.tpgi.com.au>
2017-05-09 10:52:00 +1000

    Add tests and code for PUT, POST and DELETE

--- app/controllers/cellars_controller.rb
+++ app/controllers/cellars_controller.rb
@@ -1,25 +1,42 @@
 class CellarsController < ApplicationController
-    before_action :set_cellar, only: [:show, :update, :destroy]
+  before_action :set_cellar, only: [:show, :update, :destroy]
 
-    #GET /cellars
-    def index
-        @cellars = Cellar.all
-        json_response(@cellars)
-    end
+  # GET /cellars
+  def index
+    @cellars = Cellar.all
+    json_response(@cellars)
+  end
 
-    # GET /cellars/:id
-    def show
-        json_response(@cellar)
-    end
+  # GET /cellars/:id
+  def show
+    json_response(@cellar)
+  end
 
-    private
+  #POST /cellars
+  def create
+    @cellar = Cellar.create!(cellar_params)
+    json_response(@cellar, :created)
+  end
 
-    def cellar_params
-        # whitelist params
-        params.permit(:name)
-    end
+  # PUT /cellars/:id
+  def update
+    @cellar.update(cellar_params)
+    head :no_content
+  end
 
-    def set_cellar
-        @cellar = Cellar.find(params[:id])
-    end
+  def destroy
+    @cellar.destroy
+    head :no_content
+  end
+
+  private
+
+  def cellar_params
+    # whitelist params
+    params.permit(:name)
+  end
+
+  def set_cellar
+    @cellar = Cellar.find(params[:id])
+  end
 end
--- spec/requests/cellars_spec.rb
+++ spec/requests/cellars_spec.rb
@@ -1,52 +1,108 @@
 require 'rails_helper'
 
 RSpec.describe 'Cellars API', type: :request do
-    # initialise test data
-    let!(:cellars) { create_list(:cellar, 10) }
-    let(:cellar_id) { cellars.first.id }
+  # initialise test data
+  let!(:cellars) {create_list(:cellar, 10)}
+  let(:cellar_id) {cellars.first.id}
 
-    # Test suite for GET /cellars
-    describe 'GET /cellars' do
-        
-        # make HTTP get requests before each example
-        before { get '/cellars'}
+  # Test suite for GET /cellars
+  describe 'GET /cellars' do
 
-        it 'returns cellars' do
-            expect(json).not_to be_empty
-            expect(json.size).to equal(10)
-        end
-        
-        it 'returns status code 200' do
-            expect(response).to have_http_status(200)
-        end
+    # make HTTP get requests before each example
+    before {get '/cellars'}
+
+    it 'returns cellars' do
+      expect(json).not_to be_empty
+      expect(json.size).to eq(10)
     end
 
-    # test suite for GET /cellars/:id
-    describe 'GET /cellars/:id' do
-        before { get "/cellars/#{cellar_id}" }
-
-        context 'when the record exists' do
-           
-           it 'returns the cellar' do
-               expect(json).not_to be_empty
-               expect(json['id']).to equal(cellar_id)
-           end 
-
-           it 'returns the status code 200' do
-               expect(response).to have_http_status(200)
-           end
-        end
-
-        context 'when the record does not exist' do
-            let(:cellar_id) { 101 }
-
-            it 'returns status code 404' do
-                expect(response).to have_http_status(404)
-            end
-
-            it 'returns a not found message' do
-                expect(response.body).to match(/Couldn't find Cellar/)
-            end
-        end
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
     end
+  end
+
+  # test suite for GET /cellars/:id
+  describe 'GET /cellars/:id' do
+    before {get "/cellars/#{cellar_id}"}
+
+    context 'when the record exists' do
+
+      it 'returns the cellar' do
+        expect(json).not_to be_empty
+        expect(json['id']).to eq(cellar_id)
+      end
+
+      it 'returns the status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'when the record does not exist' do
+      let(:cellar_id) {101}
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Cellar/)
+      end
+    end
+  end
+
+  # test suite for POST /cellars
+  describe 'POST /cellars' do
+    let(:valid_attributes) {{name: "Cevo Cellar"}}
+
+    context 'when the request is valid' do
+      before { post '/cellars', params: valid_attributes}
+
+      it "creates a cellar" do
+        expect(json['name']).to eq('Cevo Cellar')
+      end
+
+      it "returns status code 201" do
+        expect(response).to have_http_status(201)
+      end
+    end
+
+    context 'when the request is invalid' do
+      before { post '/cellars', params: { foo: "bar"}}
+
+      it "returns a status code 422" do
+        expect(response).to have_http_status(422)
+      end
+
+      it "returns a validation failure message" do
+        expect(response.body).to match(/Validation failed: Name can't be blank/)
+      end
+    end
+  end
+
+  # test suite for PUT /cellars/:id
+  describe 'PUT /cellars/:id' do
+    let(:valid_attributes) { { Name: "Cevo Cellar"} }
+
+    context 'when the record is valid' do
+      before { put "/cellars/#{cellar_id}", params: valid_attributes }
+
+      it "updates the record" do
+        expect(response.body).to be_empty
+      end
+
+      it "returns status code 204" do
+        expect(response).to have_http_status(204)
+      end
+
+    end
+  end
+
+  # test suite for DELETE /cellars/:id
+  describe 'Delete /cellar/:id' do
+    before { delete "/cellars/#{cellar_id}"}
+
+    it 'returns status code 204' do
+      expect(response).to have_http_status(204)
+    end
+  end
 end
