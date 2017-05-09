ec3274367d6a57460ebb4dc74b796fc079ba6b24
Rob Lugton <rob.lugton@192-168-1-91.tpgi.com.au>
2017-05-09 13:24:27 +1000

    Fix several issues in the Bottles factory class
    - Fixture::Coffee does not exist in 1.7.3 apparently
    - removed un-needed field
    - fix typo in factory declaration

--- spec/factories/bottles.rb
+++ spec/factories/bottles.rb
@@ -1,11 +1,10 @@
 
 FactoryGirl.define do
-    factory :botte do
+    factory :bottle do
         name { Faker::Book.title }
         winery { Faker::Book.publisher }
-        year { Faker::Number.number(4) }
-        description { Faker::Coffee.notes }
-        done false
+        vintage { Faker::Number.number(4) }
+        description { Faker::Hipster.sentence }
         cellar_id nil
     end
 end
