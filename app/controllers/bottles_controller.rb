db5a042d51e715e7d871fd41c0730d6e80fadcdb
Rob Lugton <rob.lugton@192-168-1-91.tpgi.com.au>
2017-05-09 13:54:42 +1000

    Tests and code for retrieving bottles from the cellar

--- app/controllers/bottles_controller.rb
+++ app/controllers/bottles_controller.rb
@@ -1,2 +1,24 @@
 class BottlesController < ApplicationController
+  before_action :set_cellar
+  before_action :set_cellar_bottle, only: [:show]
+
+  # GET /cellars/:cellar_id/bottles
+  def index
+    json_response(@cellar.bottles)
+  end
+
+  def show
+    json_response(@bottle)
+  end
+
+  private
+
+  def set_cellar
+    @cellar = Cellar.find(params[:cellar_id])
+  end
+
+  def set_cellar_bottle
+    @bottle = @cellar.bottles.find_by!(id: params[:id]) if @cellar
+  end
+
 end
--- spec/requests/bottles_spec.rb
+++ spec/requests/bottles_spec.rb
@@ -1,1 +1,51 @@
 require 'rails_helper'
+
+RSpec.describe 'Items API' do
+  let!(:cellar) { create(:cellar) }
+  let!(:bottles) { create_list(:bottle, 20, cellar_id: cellar.id)}
+  let(:cellar_id) { cellar.id }
+  let(:id) { bottles.first.id }
+
+  # Test Suite for GET /cellars/:cellar_id/bottles
+  describe 'GET /cellars/:cellar_id/bottles' do
+    before { get "/cellars/#{cellar_id}/bottles" }
+
+    context 'when cellar exists' do
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns all bottle in the cellar' do
+        expect(json.size).to eq(20)
+      end
+    end
+
+    context 'when cellar does not exist' do
+      let(:cellar_id) { 0 }
+
+      it 'returns a status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Cellar/)
+      end
+    end
+  end
+
+  # Test Suite for GET /cellars/:cellar_id/bottles/:id
+  describe 'GET /cellars/:cellar_id/bottles/:id' do
+    before { get "/cellars/#{cellar_id}/bottles/#{id}" }
+
+    context 'when cellar bottle exists' do
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns the bottle' do
+        expect(json['id']).to eq(id)
+      end
+    end
+  end
+
+end
